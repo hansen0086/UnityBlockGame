8ef74151ba527cd8c5fafc3f8c2ed2369da2d4f2
Hansen Liu <hansen0086@163.com>
2019-12-18 02:24:17 +1100

    Game added with android apk release

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public Rigidbody rb;
+    public float sidewaysForce = 500f;
+    public float forwardForce = 1000f;
+    // Start is called before the first frame update
+
+    private float width;
+    private float height;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        //touch screen control
+
+        // Handle screen touches.
+        if (Input.touchCount > 0)
+        {
+            width = (float)Screen.width / 2.0f;
+            height = (float)Screen.height / 2.0f;
+            Touch touch = Input.GetTouch(0);
+            Vector2 pos = touch.position;
+            pos.x = (pos.x - width) / width;
+            pos.y = (pos.y - height) / height;
+
+            if (pos.x >= 0.5)
+            {
+                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+            if (pos.x < 0.5)
+            {
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+        }
+
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public float initialZ;
+    public Transform player;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialZ = player.position.z;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = (player.position.z - initialZ).ToString("0");
+    }
+}
